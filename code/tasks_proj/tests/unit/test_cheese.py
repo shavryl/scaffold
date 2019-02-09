b298d5b0a5e6b998e72853e429e8bdbcb528c6a1
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-02-09 20:47:58 +0200

    test monkeypatch

---
+++ code/tasks_proj/tests/unit/test_cheese.py
@@ -0,0 +1,64 @@
+import pytest
+from tasks import cheese
+import copy
+
+
+def test_def_prefs_full():
+    cheese.write_default_cheese_preferences()
+    expected = cheese._default_prefs
+    actual = cheese.read_cheese_preferences()
+    assert expected == actual
+
+
+def test_def_prefs_change_home(tmpdir, monkeypatch):
+    monkeypatch.setenv('HOME', tmpdir.mkdir('home'))
+    cheese.write_default_cheese_preferences()
+    expected = cheese._default_prefs
+    actual = cheese.read_cheese_preferences()
+    assert expected == actual
+
+def test_def_prefs_change_expanduser(tmpdir, monkeypatch):
+    fake_home_dir = tmpdir.mkdir('home')
+    monkeypatch.setattr(cheese.os.path, 'expanduser',
+                            (lambda x: x.replace('~',
+                            str(fake_home_dir))))
+    cheese.write_default_cheese_preferences()
+    expected = cheese._default_prefs
+    actual = cheese.read_cheese_preferences()
+    assert expected == actual
+
+def test_def_prefs_change_defaults(tmpdir, monkeypatch):
+    fake_home_dir = tmpdir.mkdir('home')
+    monkeypatch.setattr(cheese.os.path, 'expanduser',
+                        (lambda x: x.replace('~',
+                        str(fake_home_dir))))
+    cheese.write_default_cheese_preferences()
+    defaults_before = copy.deepcopy(cheese._default_prefs)
+
+    monkeypatch.setitem(cheese._default_prefs, 'slicing',
+                        ['provolone'])
+    monkeypatch.setitem(cheese._default_prefs, 'spreadable',
+                        ['brie'])
+    monkeypatch.setitem(cheese._default_prefs, 'salads',
+                        ['pepper jack'])
+    defaults_modified = cheese._default_prefs
+
+    cheese.write_default_cheese_preferences()
+
+    actual = cheese.read_cheese_preferences()
+    assert defaults_modified == actual
+    assert defaults_modified != defaults_before
+
+
+
+
+
+
+
+
+
+
+
+
+
+
