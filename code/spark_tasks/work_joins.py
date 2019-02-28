5d5a5868d105d6408c7dfdf57e3c460747b7e7e8
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-02-28 08:35:36 +0200

    joins in spark

--- code/spark_tasks/work_joins.py
+++ code/spark_tasks/work_joins.py
@@ -41,4 +41,65 @@ person.join(graduateProgram, joinExpression)
 
 # or specify this explicitly by passing third parameter, the joinType:
 joinType = 'inner'
-person.join(graduateProgram, joinExpression, joinType).show()
+person.join(graduateProgram, joinExpression, joinType)
+
+# outer joins evaluate the keys in both of the DF or tables and includes
+# and joins together the rows that evaluate to true or false, if there
+# is no equivalent row in either the left or right DF, spark will insert null:
+joinType = 'outer'
+person.join(graduateProgram, joinExpression, joinType)
+
+# left outer joins evaluate the keys in both of the DF or tables and
+# includes all rows from the left DF as well as any rows in the right DF
+# that have a match in the left DF. If there is no equivalent row in
+# the right DF spark will insert null:
+joinType = 'left_outer'
+graduateProgram.join(person, joinExpression, joinType)
+
+# right outer joins evaluate the keys in both of the DF and includes
+# all rows from the right DF as well as any rows in the left DF that have a
+# match in the right DF. If there is no equivalent spark will insert null
+joinType = 'right_outer'
+person.join(graduateProgram, joinExpression, joinType)
+
+# semi joins do not actually include any values from the right DF.
+# they only compare values to see if the value exists in the second DF.
+# if the value does exist, those rows will be kept in the result, even
+# if there are duplicate keys in the left DF. Think of left semi joins
+# as filters on a DF as opposed to the function of a conventional join:
+joinType = 'left_semi'
+graduateProgram.join(person, joinExpression, joinType)
+
+gradProgram2 = graduateProgram.union(spark.createDataFrame([
+    (0, "Masters", "Duplicated Row", "Duplicated School")]))
+gradProgram2.createOrReplaceTempView("gradProgram2")
+
+gradProgram2.join(person, joinExpression, joinType)
+
+# left anti joins are the opposite of left semi joins. Like left semi joins, they
+# do not actually include any values from the right DF. They only compare values
+# to see if the value exists in the second DF. However, rather than keeping the
+# values that exist in the second DF they keep only the values that do not have a
+# corresponding key in the second DF. Think of anti joins as a NOT IN sql style filter
+joinType = 'left_anti'
+graduateProgram.join(person, joinExpression, joinType)
+
+# cross join in simplest terms are inner joins that do not specify a predicate.
+# Cross joins will join every single row in the left DF to ever single row in the
+# right DF. This will cause an absolute explosion in the number of rows contained
+# in the resulting DF.
+joinType = 'cross'
+graduateProgram.join(person, joinExpression, joinType).show()
+
+person.crossJoin(graduateProgram).show()
+
+
+
+
+
+
+
+
+
+
+
