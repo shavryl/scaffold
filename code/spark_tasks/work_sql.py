a991310673b4e1d3ae9258bccca8e506780fc8a1
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-03-05 07:57:51 +0200

    spark sql

--- code/spark_tasks/work_sql.py
+++ code/spark_tasks/work_sql.py
@@ -9,3 +9,11 @@ sc = SparkContext('local')
 spark = SparkSession(sc)
 
 
+spark.read.json("data/2015-summary.json")\
+    .createOrReplaceTempView("some_sql_view")
+
+spark.sql("""SELECT DEST_COUNTRY_NAME, sum(count)
+    FROM some_sql_view GROUP BY DEST_COUNTRY_NAME""")\
+    .where("DEST_COUNTRY_NAME like 'S%'")\
+    .where("`sum(count)` > 10")\
+    .collect()
