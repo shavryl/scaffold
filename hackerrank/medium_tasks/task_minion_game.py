323827f27ec5cec720a50298af929c9e95caea58
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-04-14 09:38:56 +0300

    minions game task

--- hackerrank/medium_tasks/task_minion_game.py
+++ hackerrank/medium_tasks/task_minion_game.py
@@ -1,6 +1,25 @@
 
 
 
+class Player():
+    points = 0
+
+    def add_point(self, num):
+        self.points = self.points + num
+
+
+class Kevin(Player):
+    letters_of_type = 'AEIOUY'
+
+
+class Stuart(Player):
+    letters_of_type = 'BCDFGHJKLMNPVWXZ'
+
+
+
+class Competition(player1, player2, string):
+    ...
+
 def generate_words(index, string):
     list_of_words = []
     word = []
@@ -33,4 +52,5 @@ def add_points(list_of_words, player):
 
 
 if __name__ == '__main__':
-    ...
+    s = input()
+    Competition(Kevin, Stuart, s)
--- tests/hackerrank/medium_tasks/test_minion_game.py
+++ tests/hackerrank/medium_tasks/test_minion_game.py
@@ -1,3 +1,17 @@
 import pytest
+from hackerrank.medium_tasks.task_minion_game import generate_words, add_points
+
+
+
+def test_generate_words(index, string):
+    ...
+
+
+
+def test_add_points(list_of_words, player):
+    ...
+
+
+
 
 
