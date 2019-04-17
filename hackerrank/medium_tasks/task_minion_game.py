cb9553d4e0771c3d4e9cbc0379bf181e3211b3bf
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-04-17 21:38:16 +0300

    player methods to make list of words

--- hackerrank/medium_tasks/task_minion_game.py
+++ hackerrank/medium_tasks/task_minion_game.py
@@ -11,21 +11,45 @@ class Player():
         self.points = self.points + num
 
     def get_index(self):
-        indexes = []
-        for letter in self.letters_of_type:
-            # there is a problem that it finds only first index
-            indexed = self.the_string.find(letter)
-            if indexed >= 0:
-                indexes.append(indexed)
-        return indexes
+        result_list = []
+        for character in self.letters_of_type:
+            index_list = []
+            for index, letter in enumerate(self.the_string):
+                if letter == character:
+                    index_list.append(index)
+                    print(index, result_list)
+            if index_list:
+                result_list.append(index_list)
+        return result_list
 
+    def build_words(self, result_list):
+        # take index in result_list
+        # by index select letter from the_string
+        # create first word with this 1 letter
+        # iterate and add next letters to it
+        # save as seperate words
+        list_of_words = []
+        for index in result_list:
+            for nested in index:
+                _next = nested + 1
+                first_letter = self.the_string[nested]
+                substring = self.the_string[_next:]
+                list_of_words.append(first_letter)
 
+                for letter in substring:
+                    # take last word in list
+                    # add letter to it and save
+                    last_word = list_of_words[-1]
+                    new_word = last_word + letter
+                    list_of_words.append(new_word)
 
-    def build_words(self, string):
-        for letter in self.letters_of_type:
-            ...
+        return list_of_words
 
-
+    def count_points(self, list_of_words):
+        # get list of words
+        # compare every word to the_string
+        # get 1 point for every equal
+        ...
 
 
 class Kevin(Player):
@@ -40,32 +64,3 @@ class Competition():
 
     def pass_it(self):
         ...
-
-def generate_words(index, string):
-    list_of_words = []
-    word = []
-    index = 2
-    string = 'BANANA'
-    for index in string:
-        ...
-    # get index as 1 word,
-    # add points for it
-    # make next word as index+next index
-    # ...
-    # but indexs shoudl be as player rules
-    # vowel or consonant
-    # ...
-    # add points for it
-    # ...
-    # calculate sum for both players
-    # players is extended counter
-    # main method is composition
-    # of process and players
-
-    list_of_words.append()
-    return list_of_words
-
-
-def add_points(list_of_words, player):
-    for word in list_of_words:
-            ...
--- tests/hackerrank/medium_tasks/test_minion_game.py
+++ tests/hackerrank/medium_tasks/test_minion_game.py
@@ -1,6 +1,13 @@
 import pytest
-from hackerrank.medium_tasks.task_minion_game import generate_words, add_points, Player
+from hackerrank.medium_tasks.task_minion_game import Player
 
+
+result_list = [
+    [0], [3, 4, 13],
+    [6, 7, 14, 15, 20, 21],
+    [10, 11],[1, 8, 16, 17],
+    [5, 9, 12], [2, 18, 19]
+]
 
 
 class TestPlayer():
@@ -10,6 +17,10 @@ class TestPlayer():
     the_string = 'ARTDDSEERSNNSDEERRTTEEW'
 
     def test_get_index(self):
-        expected = (0, 'A')
+        expected = result_list
         result = self._player.get_index()
         assert result == expected
+
+    def test_buid_words(self):
+        result = self._player.build_words(result_list)
+        assert 'A' in result
