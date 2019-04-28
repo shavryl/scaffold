c6d1e6b03acaf9f4ed2ebc7c0ecbfb873bdc88dd
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-04-28 09:42:07 +0300

    one line game changer

--- tests/hackerrank/conftest.py
+++ tests/hackerrank/conftest.py
@@ -5,6 +5,12 @@ import asyncio
 @pytest.fixture(scope='function')
 def loop():
     loop = asyncio.new_event_loop()
+    # once set_event_loop() is called, every subsequent
+    # call to asyncio.get_event_loop() will return the
+    # loop instance created in this future, and you dont
+    # have to explicitly pass the loop instance through
+    # all your function calls.
+    asyncio.set_event_loop(loop)
     try:
         yield loop
     finally:
