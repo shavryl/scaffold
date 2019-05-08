590c13b91d8dacc99c41d47cc00a3e7c4234d02a
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-05-08 08:12:45 +0300

    functools partial

--- standart_lib/algorythms/functools_practice.py
+++ standart_lib/algorythms/functools_practice.py
@@ -20,6 +20,78 @@ def show_details(name, f, is_partial=False):
 
 show_details('myfunc', myfunc)
 myfunc('a', 3)
+
+
+# the partial object does not have __name__ or __doc__ attributes
+# by default, and without those attributes, decorated functions are
+# more difficult to defug. Using update_wrapper(), copies or adds
+# attributes from the original function to the partial object.
+p1 = functools.partial(myfunc, b=4)
+show_details('raw wrapper', p1)
+print('Updating wrapper:')
+print('  assign:', functools.WRAPPER_ASSIGNMENTS)
+print('  update:', functools.WRAPPER_UPDATES)
 print()
+
+# Attributes added to the wrapper are defined in WRAPPER_ASSIGNMENTS,
+# while WRAPPER_UPDATES lists values to be modifies.
+functools.update_wrapper(p1, myfunc)
+show_details('updated wrapper', p1)
+
+
+class MyClass:
+
+    def __call__(self, e, f=6):
+        "Docstring for MyClass.__call__"
+        print('  called object with:', (self, e, f))
+
+
+def show_details2(name, f):
+    'Show details of a callable object.'
+    print('{}:'.format(name))
+    print('   object:', f)
+    print('   __name__:', end=' ')
+    try:
+        print(f.__name__)
+    except AttributeError:
+        print('(no __name__)')
+    print('  __doc__', repr(f.__doc__))
+    return
+
+
+o = MyClass()
+
+show_details2('instance', o)
+o('e goes here')
+print()
+
+p = functools.partial(o, e='default for e', f=8)
+functools.update_wrapper(p, o)
+show_details2('instance wrapper', p)
+p()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
