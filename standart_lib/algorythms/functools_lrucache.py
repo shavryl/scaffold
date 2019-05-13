1f089d518805929c6efceac0ee2ccdf3c3b2ebac
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-05-13 08:07:25 +0300

    lru_cache

--- standart_lib/algorythms/functools_lrucache.py
+++ standart_lib/algorythms/functools_lrucache.py
@@ -1,12 +1,10 @@
 import functools
 
 
-
 @functools.lru_cache()
 def expensive(a, b):
     print('expensive({}, {})'.format(a, b))
     return a * b
-
 
 MAX = 2
 
@@ -14,7 +12,7 @@ print('First set of calls:')
 for i in range(MAX):
     for j in range(MAX):
         expensive(i, j)
-print(expensive.cashe_info())
+print(expensive.cache_info())
 
 print('\nSecond set of calls:')
 for i in range(MAX + 1):
@@ -22,3 +20,12 @@ for i in range(MAX + 1):
         expensive(i, j)
 print(expensive.cache_info())
 
+print('\nClearing cache:')
+expensive.cache_clear()
+print(expensive.cache_info())
+
+print('\nThird set of calls:')
+for i in range(MAX):
+    for j in range(MAX):
+        expensive(i, j)
+print(expensive.cache_info())
