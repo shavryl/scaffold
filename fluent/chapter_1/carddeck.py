7e0c3b512072ae480338b1c16606f4c38ae25faa
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-06-01 08:40:31 +0300

    vector and dunder methods

--- fluent/chapter_1/carddeck.py
+++ fluent/chapter_1/carddeck.py
@@ -19,3 +19,9 @@ class FrenchDeck:
         return self._cards[position]
 
 
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
