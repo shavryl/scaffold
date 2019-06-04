9889cf3c866d02719bfd472e03ce563b46685d89
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-06-04 09:03:16 +0300

    bisect and options

--- fluent/chapter_2/sorting.py
+++ fluent/chapter_2/sorting.py
@@ -1,5 +1,6 @@
 import bisect
 import sys
+import random
 
 
 HAYSTACK = [1, 4, 5, 6, 8, 12, 15, 20, 21, 23, 23, 26, 29, 30]
@@ -7,11 +8,22 @@ NEEDLES = [0, 1, 2, 5, 8, 10, 22, 23, 29, 30, 31]
 
 ROW_FMT = '{0:2d} @ {1:2d}    {2}{0:<2d}'
 
+SIZE = 7
+
+random.seed(1729)
+
 def demo(bisect_fn):
     for needle in reversed(NEEDLES):
         position = bisect_fn(HAYSTACK, needle)
         offset = position * '  |'
         print(ROW_FMT.format(needle, position, offset))
+
+# interesting kinda map
+def grade(score, breakpoints=[60, 70, 80, 90], grades='FDCBA'):
+    i = bisect.bisect(breakpoints, score)
+    return grades[i]
+
+
 
 if __name__ == '__main__':
 
@@ -23,3 +35,12 @@ if __name__ == '__main__':
     print('DEMO:', bisect_fn.__name__)
     print('haystack ->', ' '.join('%2d' % n for n in HAYSTACK))
     demo(bisect_fn)
+
+    graded = [grade(score) for score in [33, 99, 77, 70, 89, 90, 100]]
+    print(graded)
+
+    my_list = []
+    for i in range(SIZE):
+        new_item = random.randrange(SIZE*2)
+        bisect.insort(my_list, new_item)
+        print('%2d ->' % new_item, my_list)
