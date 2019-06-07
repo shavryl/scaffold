8b05bbc102eaba32c1d312b6d9e1397b679cdb21
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-06-07 09:10:20 +0300

    userdict as template

---
+++ fluent/chapter_3/userdictd_example.py
@@ -0,0 +1,17 @@
+import collections
+
+
+
+class StrKeyDictBetter(collections.UserDict):
+
+    def __missing__(self, key):
+        if isinstance(key, str):
+            raise KeyError
+        return self[str(key)]
+
+    def __contains__(self, key):
+        return str(key) in self.data
+
+    def __setitem__(self, key, item):
+        self.data[str(key)] = item
+
