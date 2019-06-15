ae2cc0f348107b83da6c8e3fcb54f64f96df3561
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-06-15 11:04:21 +0300

    concreate strategies

--- fluent/chapter_6/classic_strategy_pattern.py
+++ fluent/chapter_6/classic_strategy_pattern.py
@@ -38,3 +38,52 @@ class Order:
     def __repr__(self):
         fmt = '<Order total: {:.2f} due: {:.2f}>'
         return fmt.format(self.total(), self.due())
+
+
+class Promotion(ABC):
+
+    @abstractmethod
+    def discount(self, order):
+        """Return discount as a positive dollar amount"""
+
+
+class FidelityPromo(Promotion):
+    """
+    5% discount for customers with 1000 or more fidelity points
+    """
+    def discount(self, order):
+        return order.total() * .05 if order.customer.fidelity >= 1000 else 0
+
+
+class BulkItemPromo(Promotion):
+    """
+    10% discount for each LineItem with 20 or more units
+    """
+    def discount(self, order):
+        discount = 0
+        for item in order.cart:
+            if item.quantity >= 20:
+                discount += item.total() * .1
+        return discount
+
+
+class LargeOrderPromo(Promotion):
+    """
+    7% discount for orders with 10 or more distinct items
+    """
+    def discount(self, order):
+        distinct_items = {item.product for item in order.cart}
+        if len(distinct_items) >=10:
+            return order.total() * .07
+        return 0
+
+
+
+
+
+
+
+
+
+
+
