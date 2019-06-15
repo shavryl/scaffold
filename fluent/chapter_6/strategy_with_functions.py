e38e71e51f46f65f8948c61bffc4e1f15d1c62f4
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-06-15 13:44:10 +0300

    best promo selection

--- fluent/chapter_6/strategy_with_functions.py
+++ fluent/chapter_6/strategy_with_functions.py
@@ -37,3 +37,43 @@ class Order:
     def __repr__(self):
         fmt = '<Order total: {:.2f} due: {:.2f}>'
         return fmt.format(self.total(), self.due())
+
+
+def fidelity_promo(order):
+    return order.total() * .05 if order.customer.fidelity >= 1000 else 0
+
+
+def bulk_item_promo(order):
+    discount = 0
+    for item in order.cart:
+        if item.quantity >= 20:
+            discount += item.total() * .1
+    return discount
+
+
+def large_order_promo(order):
+    distinct_items = {item.product for item in order.cart}
+    if len(distinct_items) >= 10:
+        return order.total() * .07
+    return 0
+
+
+promos = [fidelity_promo, bulk_item_promo, large_order_promo]
+
+
+def best_promo(order):
+    """
+    Select best discount available
+    """
+    return max(promo(order) for promo in promos)
+
+
+
+
+
+
+
+
+
+
+
