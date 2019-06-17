a0b12b6317c6a06cd1d063a2ccf9ffc1627f9e9b
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-06-17 14:15:40 +0300

    configured __format__

--- fluent/chapter_9/vector_example.py
+++ fluent/chapter_9/vector_example.py
@@ -34,4 +34,24 @@ class Vector2d:
     def __bool__(self):
         return bool(abs(self))
 
+    def __format__(self, format_spec=''):
+        if format_spec.endswith('p'):
+            format_spec = format_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_format = '<{}, {}>'
+        else:
+            coords = self
+            outer_format = '({}, {})'
+        components = (format(c, format_spec) for c in coords)
+        return outer_format.format(*components)
 
+    @classmethod
+    def frombytes(cls, octets):
+        # read the typecode from the first byte
+        typecode = chr(octets[0])
+        # create a memoryview from the octets binary sequence
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(*memv)
+
+    def angle(self):
+        return math.atan2(self.y, self.x)
