ed3ba44f97d20e4c4d3f1a5a02dcc24dcea9c77a
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-06-22 11:53:27 +0300

    doctests and virtualclass

---
+++ fluent/chapter_11/tombolist.py
@@ -0,0 +1,23 @@
+from random import randrange
+
+from .tombola import Tombola
+
+
+# registered as a virtual class of Tombola
+@Tombola.register
+class Tombolist(list):
+
+    def pick(self):
+        if self:
+            position = randrange(len(self))
+            return self.pop(position)
+        else:
+            raise LookupError('pop from empty TomboList')
+
+    load = list.extend
+
+    def loaded(self):
+        return bool(self)
+
+    def inspect(self):
+        return tuple(sorted(self))
