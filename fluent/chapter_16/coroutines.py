c27ab3f375bb08693412b8b6fa0bb6b2c48b9337
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-06-24 09:24:49 +0300

    coroutine return result

--- fluent/chapter_16/coroutines.py
+++ fluent/chapter_16/coroutines.py
@@ -1,5 +1,9 @@
 import inspect
 from functools import wraps
+from collections import namedtuple
+
+
+Result = namedtuple('Result', 'count average')
 
 
 def coroutine(func):
@@ -19,7 +23,32 @@ def averager():
     count = 0
     average = None
     while True:
-        term = yield average
+        term = yield
+        if term is None:
+            # to return a value a coroutine must terminate normally
+            # this is why this version has a condition to break out
+            break
         total += term
         count += 1
         average = total/count
+    # sending None terminates the loop causing coroutine to end by
+    # returning the result, as usual generator raises StopIteration
+    return Result(count, average)
+
+
+class DemoException(Exception):
+    """
+    An exception for coroutines
+    """
+
+def demo_exc_handling():
+    print(' coroutine started')
+    while True:
+        try:
+            x = yield
+        except DemoException:
+            print('Demo Exception handled. Continuing...')
+        else:
+            print('coro received: {!r}'.format(x))
+
+    raise RuntimeError('This line should never run')
