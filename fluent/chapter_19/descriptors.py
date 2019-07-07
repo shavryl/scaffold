30184ab79efdececc57510427264a8b0b35c5ff3
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-07-07 08:27:20 +0300

    decorator to configure attr name

--- fluent/chapter_19/descriptors.py
+++ fluent/chapter_19/descriptors.py
@@ -53,6 +53,15 @@ class NonBlank(Validated):
         return value
 
 
+def entity(cls):
+    for key, attr in cls.__dict__.items():
+        if isinstance(attr, Validated):
+            type_name = type(attr).__name__
+            attr.storage_name = '_{}#{}'.format(type_name, key)
+    return cls
+
+
+@entity
 class LineItem:
     description = NonBlank()
     weight = Quantity()
