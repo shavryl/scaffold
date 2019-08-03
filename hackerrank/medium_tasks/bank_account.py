b5b330177ce359a3c56bfad2625478e2471776b2
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2019-08-03 10:03:47 +0300

    bank account medium

---
+++ hackerrank/medium_tasks/bank_account.py
@@ -0,0 +1,31 @@
+# go get it -> state machine pattern
+# locks to threaded code
+# try implement sorted release recipe
+
+
+
+
+
+
+
+
+
+
+class BankAccount(object):
+    def __init__(self):
+        pass
+
+    def get_balance(self):
+        pass
+
+    def open(self):
+        pass
+
+    def deposit(self, amount):
+        pass
+
+    def withdraw(self, amount):
+        pass
+
+    def close(self):
+        pass
