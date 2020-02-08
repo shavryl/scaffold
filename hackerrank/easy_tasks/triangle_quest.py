d32055c7cb04912f9f576379f51e8351fc9b5145
Stanislav Havrylchenko <shavrylchenko@deloitte.com>
2020-02-08 15:51:37 +0200

    triangle quest

---
+++ hackerrank/easy_tasks/triangle_quest.py
@@ -0,0 +1,12 @@
+
+
+def print_triangle(num):
+    print([i * i for i in range(0, num)])
+    """
+    i * 10 + i
+    1 * 1
+    2 * 11
+    3 * 111
+    4 * 1111
+    5 * 11111
+    """
---
+++ tests/hackerrank/easy_tasks/test_triangle_quest.py
@@ -0,0 +1,6 @@
+from hackerrank.easy_tasks.triangle_quest import print_triangle
+
+
+def test_triangle():
+    print_triangle(9)
+
