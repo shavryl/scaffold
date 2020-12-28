6eeb0dcd9ed7b9b47f2805e1571db5a9ebef66f0
Havrylchenko <gavrilchenko.stanislav@gmail.com>
2020-12-28 16:47:37 +0200

    jumping clouds fix

--- hackerrank/easy_tasks/jumping_clouds.py
+++ hackerrank/easy_tasks/jumping_clouds.py
@@ -1,20 +1,15 @@
 
 
-c = [0, 1, 0, 0, 0, 0]
-
-
 def jumping(c):
-    jumps = 0
-    cloud_list = list(enumerate(c))
-    for item in cloud_list:
-        print(item)
-        check_index = item[0] + 2
-        if check_index[1] != 1:
-            jumps += 1
-            return check_index
-        else:
-            check_index + 1
-            jumps += 1
-            return check_index
-
-    return jumps
+    count = 0
+    i = 0
+    try:
+        while i < (len(c) - 1):
+            if c[i + 2] == 0:
+                i += 2
+                count +=1
+            else:
+                i -= 1
+        return count
+    except IndexError as e:
+        return count
